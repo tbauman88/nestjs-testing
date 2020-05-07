6af66a3a1efad10d7299486853e74aa7ef3823b8
Jacob Gardiner <jacob_gardiner@hotmail.ca>
2020-05-07 10:43:24 -0400

    Setup CI (#2)

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   testMatch: ["**/+(*.)+(spec|test).+(ts|js)?(x)"],
   transform: {
-    "^.+\\.(ts|js|html)$": "ts-jest"
+    "^.+\\.(ts|js|html)$": "ts-jest",
   },
   resolver: "@nrwl/jest/plugins/resolver",
   moduleFileExtensions: ["ts", "js", "html"],
-  coverageReporters: ["html"]
+  coverageReporters: ["html", "lcov", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: -40,
+    },
+  },
 };
