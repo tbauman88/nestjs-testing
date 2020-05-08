40e88cf5bb03a88aa3734e01fdb2559432595c32
Tyler Bauman <t.bauman@vehikl.com>
2020-05-08 12:32:23 -0400

    improved interfaces and mappers for dynamic providers (#5)
    Co-authored-by: MikingTheViking <kingmi123@gmail.com>

--- apps/api/src/modules/vehikl/makes/hammer-squad/hammer-squad-make.service.ts
+++ apps/api/src/modules/vehikl/makes/hammer-squad/hammer-squad-make.service.ts
@@ -1,11 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { VehiklMakeService } from '../VehiklMakeService.interface';
+import { Injectable } from "@nestjs/common";
+import {
+  VehiklMakeService,
+  ExpectedTeamMemberShape,
+} from "../VehiklMakeService.interface";
 
-export const hammerSquadKey = 'hammer-squad';
-export const hammerSquadValue = [`holy crap brian is loud`];
+export const hammerSquadKey = "hammer-squad";
+export const hammerSquadValue: ExpectedTeamMemberShape[] = [
+  {
+    name: "Brian",
+    office: hammerSquadKey,
+  },
+];
+
+interface RawHammerSquad {
+  employee: string;
+  weight: number;
+  wellFormed: boolean;
+}
+
+export const rawHammerSquadValue: RawHammerSquad[] = [
+  {
+    employee: "Brian",
+    weight: 100,
+    wellFormed: false,
+  },
+];
 @Injectable()
 export class HammerSquadMakeService implements VehiklMakeService {
+  private fetchVehikls() {
+    return rawHammerSquadValue;
+  }
+
+  private mapToExpected(data: RawHammerSquad[]): ExpectedTeamMemberShape[] {
+    return data.map((raw) => ({
+      name: raw.employee,
+      office: hammerSquadKey,
+    }));
+  }
+
   public getVehikls() {
-    return hammerSquadValue;
+    const raw = this.fetchVehikls();
+
+    return this.mapToExpected(raw);
   }
 }
